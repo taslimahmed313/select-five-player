5fa6569f85d0ca0af51a01e1acff999e2bcc31dd
Taslim <taslimahmed313@gmil.com>
2022-08-22 17:09:07 +0600

    Select and Error

--- js/main.js
+++ js/main.js
@@ -6,6 +6,26 @@ function getInputValueById(inputId){
   return inputAmount;
 }
 
+   /* Select Player List and Error Handling Function .......................... */
+ function selectBestPlayerInId(playerName, playerId){
+  /* Select Player List by Use Event Delegate................................ */
+    const itemContainer = document.getElementById("player-container");
+    const list = document.createElement("li");
+    list.innerText = playerName;
+    itemContainer.appendChild(list);
+    list.style.listStyleType = "decimal";
+    document.getElementById(playerId).disabled = true;
+
+  // Error Handling..............................................................
+    const player = document.getElementById("player-container");
+    if (player.children.length > 5) {
+      alert("You can MAX add up to five players");
+      player.removeChild(player.lastElementChild);
+      document.getElementById(playerId).disabled = false;
+    }
+  }
+  
+
   /* Get and Set Player Expenses............................................ */
 document.getElementById("calculate").addEventListener('click', function(){
     const perPlayerAmount = getInputValueById('per-player-amount');
@@ -35,4 +55,24 @@ document.getElementById("calculate-total").addEventListener('click', function(){
     const total = PlayerExpenses + managerCost + coachCost;
     totalField.innerText = total;
 });
+
+  /* Get Element by Id and  Select Player List and Error Handling........ */
+  document.getElementById("messi-added").addEventListener("click", function () {
+    selectBestPlayerInId("Lionel Messi", 'messi-added');
+  });
+  document.getElementById("neymar-add").addEventListener("click", function () {
+    selectBestPlayerInId("Neymar Jr", "neymar-add");
+  });
+  document.getElementById("mbappe-add").addEventListener("click", function () {
+    selectBestPlayerInId("Kylian Mbappé",'mbappe-add');
+  });
+  document.getElementById("ramos-add").addEventListener("click", function () {
+    selectBestPlayerInId("Sergio Ramos",'ramos-add');
+  });
+  document.getElementById("slah-add").addEventListener("click", function () {
+    selectBestPlayerInId("M Salah",'slah-add');
+  });
+  document.getElementById("benzema-add").addEventListener("click", function () {
+    selectBestPlayerInId("Karim Benzema", 'benzema-add');
+  });
 
